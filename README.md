d0209bd16b048c969fb6ad410a66d27cb27f4bad
Kotlintpoint <ankit.sodha123@gmail.com>
2025-03-27 19:21:39 +0530

    Explicit Animation

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # Flutter Meals App
 
-A new Flutter project.
+## This project includes 
 
-## Getting Started
+1. GridView
+2. Widgets vs Screens
+3. How to make Widget Tappable with InkWell
+4. Cross-Screen Navigation
+5. Passing Data to the Target Screen
+6. Stack Widget
+7. Tab-based Navigation
+8. Side Drawer
+9. Filter Options
+10. Returning Data when Leaving a Screen using PopScope
+11. Reading & Using Returned Data
+12. State management using Riverpod
+13. Animations
+    - Explicit vs Implicit animation
+        | Explicit                              | Implicit    |
+        | --------------------------            |--------------------------|
+        | You control the entire animation      | Flutter controls the animation |
+        | More control but also more complexity | Less control and therefore less complexity |
+        | Can often be avoided (by using pre-built Widgets) | Use pre-built animation widgets as often as possible      | 
+        | | |
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+    - Building Custom Animation
+    - Using Built-in Animation Widgets
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,24 +5,59 @@ import 'package:flutter_meals_app/models/meal.dart';
 import 'package:flutter_meals_app/screens/meals.dart';
 import 'package:flutter_meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
- final List<Meal> availableMeals;
+  final List<Meal> availableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  // Explicit Animations
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    // start animation using forward, repeat methods
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredMeals =
+        widget.availableMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -33,10 +68,37 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList();
           for (final category in availableCategories)
-            CategoryGridItem(category: category, onSelectCategory: (){
-              _selectCategory(context, category);
-            },),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            ),
         ],
-      );
+      ),
+      /*   builder:
+             (context, child) => Padding(
+               padding: EdgeInsets.only(
+                 top: 100 - _animationController.value * 100,
+               ),
+               child: child,
+             ),
+        );
+       */
+      // more control using SlideTransition
+      builder:
+          (context, child) => SlideTransition(
+            position: Tween(
+              begin: const Offset(0, 0.30),
+              end: const Offset(0, 0),
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              ),
+            ),
+            child: child,
+          ),
+    );
   }
 }
